7e6f961dd78a3a3c81d33d85fff000c92f95e4c3
maslabgamer <samuelhasenbalg@gmail.com>
2025-05-13 11:42:25 -0700

    Add ArtilleryBPLibs include for ThistleInject and Dispatch

--- Thistle/Source/ThistleRuntime/Private/ThistleInject.cpp
+++ Thistle/Source/ThistleRuntime/Private/ThistleInject.cpp
@@ -1,6 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ThistleInject.h"
+
+#include "ArtilleryBPLibs.h"
 #include "ArtilleryDispatch.h"
 #include "UFireControlMachine.h"
 
